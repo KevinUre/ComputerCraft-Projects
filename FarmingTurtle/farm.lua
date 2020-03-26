a82eafdf958afdcc35b4717964b606cda8097684
Kevin Ure <Kevin.M.Ure@gmail.com>
2020-03-26 16:53:03 -0400

    farming turtle can calibrate zero, so it can be run on startup now

--- FarmingTurtle/farm.lua
+++ FarmingTurtle/farm.lua
@@ -1,4 +1,69 @@
-waittime = 30
+waittime = 5
+minFuel = 25
+
+function CalibrateFromAboveChest(tries)
+    if tries > 1 then
+        printf("I'm lost! Shutting down...")
+        exit()
+    end
+    local moved = turtle.forward()
+    if moved == true then
+        local success, info = turtle.inspectDown()
+        if success == true then
+            -- are we above chest?
+            if info["name"] == "minecraft:chest" then
+                turtle.turnLeft() --ahead front and we are ready
+                return true
+            --are we above a crop?
+            elseif info["name"] == "minecraft:wheat" then
+                turtle.back() --we were already good, go back
+                return true
+            end
+        end
+        -- the easy cases weren't it, so we wandered away from the farm: go back, turn, and try again
+        turtle.back()
+        turtle.turnLeft()
+        return CalibrateFromAboveChest(tries + 1)
+    else
+        -- we were obstructed from leaving the chest, turn and try again
+        turtle.turnLeft()
+        return CalibrateFromAboveChest(tries + 1)
+    end
+    return false --should never get here
+end
+
+function CalibrateFromCrops()
+    local tries = 0
+    while tries < 10 do
+        local success, info = turtle.inspectDown()
+        if success == true then
+            if info["name"] == "minecraft:chest" then
+                turtle.turnLeft()
+                turtle.forward()
+                turtle.turnLeft()
+                return
+            end
+        end
+        turtle.forward()
+        tries = tries + 1
+    end
+    printf("I'm lost! Shutting down...")
+    exit()
+end
+
+function BeginCalibration()
+    local success, info = turtle.inspectDown()
+    if success == true then
+        if info["name"] == "minecraft:chest" then
+            CalibrateFromAboveChest(0)
+        else
+            CalibrateFromCrops()
+        end
+    else
+        printf("I'm Probably above an unplanted crop block")
+        CalibrateFromCrops()
+    end
+end
 
 function findSeeds()
     for i=1,16 do 
@@ -68,7 +133,7 @@ function doFarming()
 end
 
 function manageFuel()
-    if turtle.getFuelLevel() < 25 then
+    if turtle.getFuelLevel() < minFuel then
         local fuelIndex = findFuel()
         if findFuel == 0 then
             print("need refueling")
@@ -96,6 +161,7 @@ function doALine()
 end
 
 function mainFunction()
+    BeginCalibration()
     while true do
         doALine()
     end
